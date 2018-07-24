8097858b81e86b0ab6cc08e20ab90fbe12f22a11
yangbin <yss920@163.com>
2018-07-24 09:22:56 +0000

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user', 'category')->paginate(30);
+        // print_R($topics);die;
 		return view('topics.index', compact('topics'));
 	}
 
